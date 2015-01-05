2ce5740f57a4d541d08f274cebec78ce6fa632b3
wangwenmin <xiaaiwuyi@sina.com>
2015-01-05 11:56:28 +0800

    修改eventbox

--- client/common.c
+++ client/common.c
@@ -10,16 +10,16 @@ GtkEventBox *BuildEventBox(GtkWidget *warp, GCallback press, GCallback enter, GC
                     | GDK_POINTER_MOTION_MASK | GDK_POINTER_MOTION_HINT_MASK);
     if (press)
         g_signal_connect(G_OBJECT(eventBox), "button_press_event",
-                G_CALLBACK(press), NULL);       // 加入事件回调
+                G_CALLBACK(press), data);       // 加入事件回调
     if (enter)
         g_signal_connect(G_OBJECT(eventBox), "enter_notify_event",
-                G_CALLBACK(enter), NULL);
+                G_CALLBACK(enter), data);
     if (release)
         g_signal_connect(G_OBJECT(eventBox), "button_release_event",
-                G_CALLBACK(release), NULL);
+                G_CALLBACK(release), data);
     if (leave)
         g_signal_connect(G_OBJECT(eventBox), "leave_notify_event",
-                G_CALLBACK(leave), NULL);
+                G_CALLBACK(leave), data);
     GdkRGBA rgba = {1, 1, 1, 0};
     gtk_widget_override_background_color(GTK_WIDGET(eventBox), GTK_STATE_FLAG_NORMAL, &rgba);//设置透明
     gtk_container_add((GTK_CONTAINER(eventBox)), warp);
