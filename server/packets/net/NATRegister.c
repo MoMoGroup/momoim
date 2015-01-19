553c07efcc97a64b81b2e6f54eea11adcd327616
xuan <xuanmgr@gmail.com>
2015-01-19 13:31:39 +0800

    NAT穿透

--- server/packets/net/NATRegister.c
+++ server/packets/net/NATRegister.c
@@ -34,7 +34,7 @@ static int DiscoverDetected(const struct sockaddr_in *addr, void *data)
     if (user)
     {
         struct sockaddr_in fromAddr;
-        socklen_t len;
+        socklen_t len = sizeof(fromAddr);
         getpeername(user->crp->fd, &fromAddr, &len);
         log_info("Discover",
                  "UID:%u,Session:%u,From:%s,To:%s\n",
