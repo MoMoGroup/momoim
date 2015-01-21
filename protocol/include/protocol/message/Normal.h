beeaa63ecf52ab76bb9e6154aee82abe19cb6e7b
xia <453993887@qq.com>
2015-01-21 10:41:10 +0800

    离线消息等显示为服务器时间

--- protocol/include/protocol/message/Normal.h
+++ protocol/include/protocol/message/Normal.h
@@ -9,6 +9,7 @@ CRP_STRUCTURE
     uint32_t uid;
     uint8_t messageType;
     //USER_MESSAGE_TYPE
+    time_t time;
     uint16_t messageLen;
     char message[0];
 } CRPPacketMessageNormal;
@@ -16,4 +17,10 @@ CRP_STRUCTURE
 __attribute_malloc__
 CRPPacketMessageNormal *CRPMessageNormalCast(CRPBaseHeader *base);
 
-int CRPMessageNormalSend(CRPContext context, uint32_t sessionID, USER_MESSAGE_TYPE messageType, uint32_t uid, uint16_t messageLen, char *message);
+int CRPMessageNormalSend(CRPContext context,
+                         uint32_t sessionID,
+                         USER_MESSAGE_TYPE messageType,
+                         uint32_t uid,
+                         time_t time,
+                         uint16_t messageLen,
+                         char *message);
